4d7f8ce41e26a8ab2507e2e561bc794f20b791d7
gauravtiwari2509 <gauravtiwari2509@gmail.com>
2025-07-12 11:38:18 +0530

    correct schema added password field and signup feature added

---
+++ lib/auth/authOptions.ts
@@ -0,0 +1,74 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/db/prisma";
+import bcrypt from "bcryptjs";
+import { NextAuthOptions } from "next-auth";
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!isValid) {
+          throw new Error("password incorrect");
+        }
+
+        if (!user.verified) {
+          throw new Error("account not verified, signup again");
+        }
+        return {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+        token.email = user.email;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+        session.user.email = token.email;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
